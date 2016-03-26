8df67432d2dd57f5589aae6a56889761ab756ba6
Kusubooru <kusubooru@gmail.com>
2016-03-26 18:10:32 +0200

    Add User struct

---
+++ teian/teian.go
@@ -0,0 +1,13 @@
+package teian
+
+import "time"
+
+// User represents a user of the application.
+type User struct {
+	ID       int
+	Name     string
+	Pass     string
+	JoinDate *time.Time
+	Admin    string
+	Email    string
+}
