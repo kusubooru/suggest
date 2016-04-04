8db5f6052ce0d83f94d15227d4d9dfe3d0e9452b
Kusubooru <kusubooru@gmail.com>
2016-04-04 04:10:17 +0300

    Add missing documentation in teian

--- teian/teian.go
+++ teian/teian.go
@@ -25,28 +25,37 @@ type Sugg struct {
 	Created  time.Time
 }
 
+// FmtCreated returns the creation time of the suggestion formatted as:
+//
+//     Mon 02 Jan 2006 15:04:05 MST
 func (s *Sugg) FmtCreated() string {
 	return s.Created.UTC().Format("Mon 02 Jan 2006 15:04:05 MST")
 }
 
+// ByDate helps to sort suggestions by creation time.
 type ByDate []Sugg
 
 func (s ByDate) Len() int           { return len(s) }
 func (s ByDate) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s ByDate) Less(i, j int) bool { return s[i].Created.Before(s[j].Created) }
 
+// ByUser helps to sort suggestions by username.
 type ByUser []Sugg
 
 func (s ByUser) Len() int           { return len(s) }
 func (s ByUser) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s ByUser) Less(i, j int) bool { return s[i].Username < s[j].Username }
 
+// ByID helps to sort suggestions by ID.
 type ByID []Sugg
 
 func (s ByID) Len() int           { return len(s) }
 func (s ByID) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s ByID) Less(i, j int) bool { return s[i].ID < s[j].ID }
 
+// FindByID first sorts the slice of suggestions by ID and then searches for
+// the given id. If the id is found in the slice then it returns the index and
+// the suggestion. If the id is not found then it returns index -1 and nil.
 func FindByID(suggs []Sugg, id uint64) (int, *Sugg) {
 	sort.Sort(ByID(suggs))
 	i := sort.Search(len(suggs), func(i int) bool { return suggs[i].ID >= id })
@@ -56,6 +65,8 @@ func FindByID(suggs []Sugg, id uint64) (int, *Sugg) {
 	return -1, nil
 }
 
+// FilterByUser returns suggestions whose Username contains the provided
+// username. It is used to filter a slice of suggestions by username.
 func FilterByUser(suggs []Sugg, username string) []Sugg {
 	var f []Sugg
 	for _, s := range suggs {
@@ -66,6 +77,9 @@ func FilterByUser(suggs []Sugg, username string) []Sugg {
 	return f
 }
 
+// FilterByText returns suggestions whose Text contains the provided text. It
+// is used to search a slice of suggestions and return only those containing
+// the given text.
 func FilterByText(suggs []Sugg, text string) []Sugg {
 	var f []Sugg
 	for _, s := range suggs {
