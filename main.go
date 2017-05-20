7a0bd02daaf61e9477429080484727a6e118ff6a
Kusubooru <kusubooru@gmail.com>
2017-05-20 12:52:00 +0300

    Allow CORS on test login handler

--- main.go
+++ main.go
@@ -97,6 +97,7 @@ func main() {
 	http.Handle("/suggest/submit", shim.Auth(app.handleSubmit, *loginURL))
 	http.Handle("/suggest/login", http.HandlerFunc(app.serveLogin))
 	http.Handle("/suggest/login/submit", http.HandlerFunc(app.handleLogin))
+	http.Handle("/suggest/login/test", allowCORS(http.HandlerFunc(app.testLogin)))
 	http.Handle("/suggest/logout", http.HandlerFunc(handleLogout))
 
 	if useTLS {
@@ -245,6 +246,28 @@ func (app *App) handleLogin(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/suggest", http.StatusFound)
 }
 
+// allowCORS allows Cross Origin Resource Sharing from any origin.
+// Don't do this without consideration in production systems.
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+				return
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+	return
+}
 
 func (app *App) testLogin(w http.ResponseWriter, r *http.Request) {
 	// only accept POST method
